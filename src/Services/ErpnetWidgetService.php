9fc3dd9b8b3fc32c853ca3ec8691d8b1a21cba9a
Luciano Porto <luciano.bapo@gmail.com>
2016-08-27 10:08:06 -0300

    first commit

---
+++ src/Services/ErpnetWidgetService.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luciano
+ * Date: 27/08/16
+ * Time: 09:53
+ */
+
+namespace ErpNET\WidgetResource\Services;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class ErpnetWidgetService
+{
+    /**
+     * @param array $dataArray
+     * @param Model $dataModelInstance
+     * @param string $routePrefix
+     * @param array $fieldsConfig
+     * @param bool|false $hasFiles
+     * @param string $method
+     * @param Model|null $dataModelSelected
+     * @return \Illuminate\View\View|\Illuminate\Contracts\View\Factory
+     */
+    public function widgetDataIndex(array $dataArray = [],
+                           $dataModelInstance, $routePrefix, array $fieldsConfig,
+                           $hasFiles = false, $method = 'POST', $dataModelSelected = null){
+//        $this->authorize($jokeService->dataModelInstance());
+
+//        $fields = $jokeService->getFillableFields();
+//        foreach($fields as $key => $field){
+//            if ($field=='file'){
+//                $fields[$key]=[
+//                    'name' => 'file',
+//                    'component' => 'customFile',
+//                ];
+//            };
+//        }
+        return view('erpnetWidgetResource::dataIndex')->with([
+            'data' => $dataArray,
+//            'dataModelSelected' => $dataModelSelected,
+            'dataModelInstance' => $dataModelInstance,
+            'routePrefix' => $routePrefix,
+            'fields' => $fieldsConfig,
+            'customFormAttr' => [
+                'route' => [
+                    $routePrefix.'.store',
+//                    'joke'=>$joke,
+                ],
+                'files' => $hasFiles,
+                'method' => $method,
+            ],
+        ]);
+    }
+}
