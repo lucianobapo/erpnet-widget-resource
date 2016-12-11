96882345fe927dae050b03eb38025baf3e9f4f41
Luciano Porto <luciano.bapo@gmail.com>
2016-12-10 23:17:31 -0200

    automatic-commit-alias

--- resources/views/dataIndexLayout4.blade.php
+++ resources/views/dataIndexLayout4.blade.php
@@ -41,12 +41,12 @@
                                             <a class="btn btn-primary" title="{{ t('More Data') }}">
                                                 <i class="fa fa-plus" aria-hidden="true"></i>
                                             </a>
-                                            <a href="{{ route($routePrefix.'.show', [$item['id'], '#form']) }}" class="btn btn-primary" title="{{ t('Edit') }}">
+                                            <a href="{{ route($routePrefix.'.edit', [$item['id'], '#form']) }}" class="btn btn-primary" title="{{ t('Edit') }}">
                                                 <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
                                             </a>
 
                                             {{ Form::open(['style'=> 'display: inline-block;', 'method' => 'DELETE', 'route' => [$routePrefix.'.destroy', $item['id']] ]) }}
-                                                <button type="submit" class="btn btn-danger" title="{{ t('Cancel') }}">
+                                                <button type="submit" class="btn btn-danger" title="{{ t('Delete') }}">
                                                     <i class="fa fa-trash" aria-hidden="true"></i>
                                                 </button>
                                             {{ Form::close() }}
