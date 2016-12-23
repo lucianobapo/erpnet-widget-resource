84e1814be2482d4e98cc16397ea7fc7063f155e2
Luciano Porto <luciano.bapo@gmail.com>
2016-12-22 22:06:10 -0200

    automatic-commit-alias

--- resources/views/dataIndexLayout4.blade.php
+++ resources/views/dataIndexLayout4.blade.php
@@ -41,11 +41,11 @@
                                             <a class="btn btn-primary" title="{{ t('More Data') }}">
                                                 <i class="fa fa-plus" aria-hidden="true"></i>
                                             </a>
-                                            <a href="{{ Route::getRoutes()->hasNamedRoute($routePrefix.'.edit')?route($routePrefix.'.edit', [$item['id'], '#form']):route($routePrefix.'.show', [$item['id'], '#form']) }}" class="btn btn-primary" title="{{ t('Edit') }}">
+                                            <a href="{{ Route::getRoutes()->hasNamedRoute($routePrefix.'.edit')?route($routePrefix.'.edit', [$item[$item->getRouteKeyName()], '#form']):route($routePrefix.'.show', [$item['id'], '#form']) }}" class="btn btn-primary" title="{{ t('Edit') }}">
                                                 <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
                                             </a>
 
-                                            {{ Form::open(['style'=> 'display: inline-block;', 'method' => 'DELETE', 'route' => [$routePrefix.'.destroy', $item['id']] ]) }}
+                                            {{ Form::open(['style'=> 'display: inline-block;', 'method' => 'DELETE', 'route' => [$routePrefix.'.destroy', $item[$item->getRouteKeyName()]] ]) }}
                                                 <button type="submit" class="btn btn-danger" title="{{ t('Delete') }}">
                                                     <i class="fa fa-trash" aria-hidden="true"></i>
                                                 </button>
